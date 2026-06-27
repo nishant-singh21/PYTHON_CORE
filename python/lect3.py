dfedb6b3127440a45943929a270483405a5052af
nishant-singh21 <nishantsingh1721@gmail.com>
2026-06-27 12:31:13 +0530

    py lect3 comp

---
+++ python/lect3.py
@@ -0,0 +1,34 @@
+# name = "mohit"  that is an comment functions 
+'''  that is multiple line comments '''
+
+# output
+name = "mohit"
+print("Name:", name )
+
+
+# input
+name  = int(input("Enter your Age : "))
+
+
+
+print("Age:", age )
+print("type of  age": type(age))
+
+# that input will store the variable in the input
+
+
+# formating of output
+# comma
+name = "mohit"
+print("My Name is",name)
+
+# f string  recommand method of python 
+name = "mohit"
+age = 20
+print(f"My Name: {name} \n My  Age :{age}")
+
+
+# format method 
+print("MY Name is {} and my age is {}". format(name,age))
+
+
