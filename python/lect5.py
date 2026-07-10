0edd75ba9c5583cb07e7bcd22cc21b812d56e495
nishant-singh21 <nishantsingh1721gmail.com>
2026-07-10 09:21:11 +0530

    change

--- python/lect5.py
+++ python/lect5.py
@@ -2,33 +2,33 @@
 
 # # basic operations
 
-# str1=" Hello \n world"
-# str2='Hello world'
-# str3=''' Hello world'''
-# print(str1)
-# print(str2)
-# print(str3)
+str1=" Hello \n world"
+str2='Hello world'
+str3=''' Hello world'''
+print(str1)
+print(str2)
+print(str3)
 
 
 # # concatenation means adding of two string 
 
-# str1="Hello"
-# str2="World"
-# strFinal=str1+" "+str2
-# print(strFinal) # Hello World
+str1="Hello"
+str2="World"
+strFinal=str1+" "+str2
+print(strFinal) # Hello World
 
 
 
 # # String length
-# str="Hello World"
-# print(len(str)) # 11
+str="Hello World"
+print(len(str)) # 11
 
 # # indexing of string 
 
-# print(str[0]) # H
+print(str[0]) # H
 
-# print(str[6]) # W
-# print(str[10]) # d
+print(str[6]) # W
+print(str[10]) # d
 
 
 # slicing of string
