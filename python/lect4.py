6696730318353e36c6298374efc623e5761fff7e
nishant-singh21 <nishantsingh1721gmail.com>
2026-07-09 20:39:37 -0700

    not done

---
+++ python/lect4.py
@@ -0,0 +1,39 @@
+# Arithmatic operator 
+num1 = 20 
+num2 = 10
+
+print("Addition:" ,num1 + num2)
+print("Subtraction:" ,num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2 )
+print("Floor Division", num1 // num2)
+print("Modulus:", num1 % num2)
+print("Exponentiation:", num1 ** num2)
+
+
+# comparison operator operator // Relational operator 
+num1 = 2
+num2 = 4
+print("Equal to:" , num1 == num2)
+print("Not Equal to:" , num1 != num2)
+print("Greater than:", num1>num2)
+print("lesss than:" , num1<num2)
+print("greater or equal to:" , num1>=num2)
+print("lessor or equal to:" , num1<=num2)
+
+
+# Logical operator // Boolean operator
+num1=  True
+num2= False
+
+print("Logical AND:", num1 and num2)
+print("Logical OR:", num1 or num2)
+print("logical NOT:", not num1>num2)
+
+
+# Assignment operator
+
+num = 2 
+# num =4
+num += num 
+print("num:", num)
---
+++ python/lect5.py
@@ -0,0 +1,24 @@
+# Strings 
+
+# # basic operations
+
+# str1=" Hello \n world"
+# str2='Hello world'
+# str3=''' Hello world'''
+# print(str1)
+# print(str2)
+# print(str3)
+
+
+# # concatenation means adding of two string 
+
+# str1="Hello"
+# str2="World"
+# strFinal=str1+" "+str2
+# print(strFinal) # Hello World
+
+
+
+# String length
+str="Hello World"
+print(len(str)) # 11
