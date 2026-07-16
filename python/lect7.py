6329668cdb1b41ad232ec7d3475b728223077a22
nishant-singh21 <122474129+nishant-singh21@users.noreply.github.com>
2026-07-16 19:34:40 +0530

    list  data type

---
+++ python/lect7.py
@@ -0,0 +1,21 @@
+# list in python 
+# creation list , accesing list items , changinglist item 
+# slicing list , using list methods 
+
+# creation of list 
+num =[1,"Nishant",3.5,True]
+num1=[] # also run empty list
+print(num)
+print(type(num))
+
+# accessing list items
+num =[10,20,30,40,50]
+
+print(num[0]) # accessing first item
+print(num[1]) # accessing second item
+
+# changing list item
+num[0] = 100    
+
+print(len(num)) # length of list
+print(num) # print the modified list
